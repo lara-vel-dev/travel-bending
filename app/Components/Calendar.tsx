747507050acc32de59f8f740f56ee4eccb86b666
Daniela Lara <laradni@icloud.com>
2023-06-08 23:45:58 -0600

    (Add) Prisma client

--- app/Components/Calendar.tsx
+++ app/Components/Calendar.tsx
@@ -1,7 +1,35 @@
-const Calendar = () => {
+"use client";
+
+import ReactCalendar from "react-calendar";
+import { useState, FC } from "react";
+
+interface CalendarProps {}
+
+interface DateType {
+  justDate: Date | null;
+  dateTime: Date | null;
+}
+
+const Calendar: FC<indexProps> = ({}) => {
+  const [date, setDate] = useState<DateType>({
+    justDate: null,
+    dateTime: null,
+  });
+
   return (
-    <div>
-      <h1>Calendar</h1>
+    <div className="flex h-screen flex-col items-center justify-center">
+      {date.justDate ? (
+        <div></div>
+      ) : (
+        <ReactCalendar
+          minDate={new Date()}
+          className="react-calendar p-2"
+          view="month"
+          onClickDay={(date) =>
+            setDate((prev) => ({ ...prev, justDate: date }))
+          }
+        />
+      )}
     </div>
   );
 };
