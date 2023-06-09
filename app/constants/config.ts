e9a55c35205cbbb90ecd3c2b1cb736b8054b7acf
Daniela Lara <laradni@icloud.com>
2023-06-09 09:17:23 -0600

    (Del) Constants file and Calendar component

--- app/constants/config.ts
+++
@@ -1,6 +0,0 @@
-export const FIRST_FLIGHT = 5; // in hours
-export const LAST_FLIGHT = 23; // in hours
-export const INTERVAL = 45; // in minutes
-
-export const CATEGORIES = ["fire", "earth", "water", "air"];
-export const NOW = new Date();
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-// Imports
-import Calendar from "./Components/Calendar";
-
 const Home = () => {
   return (
     <div>
-      <h1>Hello!!</h1>
-      <Calendar />
+      
     </div>
   );
 };
