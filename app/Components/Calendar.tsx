ba5612cb3247ff54c749af4c7d1120ba4f766af7
Daniela Lara <laradni@icloud.com>
2023-06-09 00:37:31 -0600

    (Mod) Calendar Component

--- app/Components/Calendar.tsx
+++ app/Components/Calendar.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { format, add } from "date-fns";
 import ReactCalendar from "react-calendar";
-import { useState, FC } from "react";
+import { useEffect, useState, type FC } from "react";
+import { INTERVAL, FIRST_FLIGHT, LAST_FLIGHT } from "../constants/config";
 
 interface CalendarProps {}
 
@@ -10,16 +12,44 @@ interface DateType {
   dateTime: Date | null;
 }
 
-const Calendar: FC<indexProps> = ({}) => {
+const Calendar: FC<CalendarProps> = ({}) => {
   const [date, setDate] = useState<DateType>({
     justDate: null,
     dateTime: null,
   });
 
+  const getTimes = () => {
+    if (!date.justDate) return;
+
+    const { justDate } = date;
+
+    const beggining = add(justDate, { hours: FIRST_FLIGHT });
+    const end = add(justDate, { hours: LAST_FLIGHT });
+
+    const times = [];
+    for (let i = beggining; i <= end; i = add(i, { minutes: INTERVAL })) {
+      times.push(i);
+    }
+    return times;
+  };
+
+  const times = getTimes();
+
   return (
     <div className="flex h-screen flex-col items-center justify-center">
       {date.justDate ? (
-        <div></div>
+        <div className="flex gap-4">
+          {times?.map((time, i) => (
+            <div key={`time-${i}`} className="rounded-sm bg-gray-100 p-2">
+              <button
+                type="button"
+                onClick={() => setDate((prev) => ({ ...prev, dateTime: time }))}
+              >
+                {format(time, "kk:mm")}
+              </button>
+            </div>
+          ))}
+        </div>
       ) : (
         <ReactCalendar
           minDate={new Date()}
