a107683febf931224e690aa60c8a347bf767912d
Daniela Lara <laradni@icloud.com>
2023-06-09 00:38:16 -0600

    (Add) Constants file

---
+++ app/constants/config.ts
@@ -0,0 +1,6 @@
+export const FIRST_FLIGHT = 5; // in hours
+export const LAST_FLIGHT = 23; // in hours
+export const INTERVAL = 45; // in minutes
+
+export const CATEGORIES = ["fire", "earth", "water", "air"];
+export const NOW = new Date();
