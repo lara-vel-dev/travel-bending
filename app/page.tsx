245d8951edf39eea887b99e296f7f50d107b71ed
Daniela Lara <laradni@icloud.com>
2023-06-26 17:53:58 -0600

    (Mod) Main file

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import Image from "next/image";
+import homeImage from './img/fire.png';
+
 const Home = () => {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <>
+      <Image src={homeImage}
+      alt='Fire nation'
+      style={{
+        width: '100%',
+        height: '450px',
+        objectFit: 'cover',
+      }}/>
+    </>
   );
 };
 
